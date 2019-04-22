e74c348d1fff9b5689200a26c680067de8ad4994
chax <chackop@gmail.com>
2019-04-22 19:27:29 +0100

    Update solution.mjs
    started serializer

--- test/solution.mjs
+++ test/solution.mjs
@@ -1,2 +1,18 @@
-function add(...args) { return args.reduce((num, tot) => tot + num) };
-module.exports = { add };
+export default {
+    add(...args) {
+        return args.reduce((num, tot) => tot + num)
+    },
+    serialize(...args) {
+        let newObj = {};
+        args.row && args.row.forEach((data, index) => {
+            for (const key in data) {
+                if (data.hasOwnProperty('hits')) {
+                    newObj['hits' + index + _ + key] = data[key];
+                } else {
+                    newObj['row' + index + _ + key] = data[key];
+                }
+            }
+        });
+        return newObj;
+    }
+};
